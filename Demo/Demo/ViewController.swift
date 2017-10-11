aa9e69995d806ba0407e3c708c222021dcf11da8
ZeroOneMore <1049235238@qq.com>
2017-10-11 16:30:54 +0800

    1.0.1

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -22,31 +22,32 @@ class ViewController: UIViewController,YVImagePickerControllerDelegate {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        /*
-         照片
-         每行4列
-         单选
-         */
-        let pickerVC = YVImagePickerController()
-        pickerVC.yvmediaType = .image
-        pickerVC.yvcolumns = 4
-        pickerVC.yvIsMultiselect = false
-        pickerVC.delegate = self
-        self.present(pickerVC, animated: true, completion: nil)
-        
         
         /*
          照片
          每行5列
          多选
          */
+        let pickerVC = YVImagePickerController()
+        pickerVC.yvmediaType = .image
+        pickerVC.yvcolumns = 5
+        pickerVC.yvIsMultiselect = true
+        pickerVC.delegate = self
+        self.present(pickerVC, animated: true, completion: nil)
+        
+//        /*
+//         照片
+//         每行4列
+//         单选
+//         */
 //        let pickerVC = YVImagePickerController()
 //        pickerVC.yvmediaType = .image
-//        pickerVC.yvcolumns = 5
-//        pickerVC.yvIsMultiselect = true
+//        pickerVC.yvcolumns = 4
+//        pickerVC.yvIsMultiselect = false
 //        pickerVC.delegate = self
 //        self.present(pickerVC, animated: true, completion: nil)
-        
+//        
+    
         
         /*
          视频
@@ -67,23 +68,25 @@ class ViewController: UIViewController,YVImagePickerControllerDelegate {
     func yvimagePickerController(_ picker: YVImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
          picker.dismiss(animated: true, completion: nil)
-        //单选照片
-        if info["imagedata"] != nil{
-            let image =  info["imagedata"] as! UIImage
-            print("\(image)")
+        
+        //多选照片
+        if info["imagedatas"] != nil{
+            let phassets = info["imagedatas"] as! Array<PHAsset>
             
+              print("\(phassets)")
         }
+//        //单选照片
+//       else  if info["imagedata"] != nil{
+//            let image =  info["imagedata"] as! UIImage
+//            print("\(image)")
+//            
+//        }
 //        //单选视频
 //        else  if info["videodata"] != nil{
 //            let url = info["videodata"] as! URL
 //            
 //        }
-//        //多选照片
-//        else  if info["imagedatas"] != nil{
-//            let phassets = info["imagedatas"] as! Array<PHAsset>
-//            
-//            
-//        }
+     
         
     }
     func yvimagePickerControllerDidCancel(_ picker: YVImagePickerController) {
